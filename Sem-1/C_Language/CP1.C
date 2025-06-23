c63900fb40df7cda61ac7ac8a9816a4c6a51118d
Chudasama Vishal <chudasamavishal183@gmail.com>
2025-06-23 22:04:46 +0530

    first day first c program
    c program to print name, number and address

---
+++ Sem-1/C_Language/CP1.C
@@ -0,0 +1,13 @@
+//This is simple c program for print name, number and address
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	clrscr();
+	printf("\n\n\tName\t: Chudasama Vishal");
+	printf("\n\n\tNumber\t: 8320343610");
+	printf("\n\n\tAddress\t: Kotadiya, Khambhaliya, Devbhumi Dwarka - 361306");
+	getch();
+	return 0;
+}
