168661f594f42ad14f9b2a425d85fc3490bd753e
Chudasama Vishal <chudasamavishal183@gmail.com>
2025-09-28 23:24:20 +0530

    fix it, make more good

--- Sem-1/C_Language/PRIME.C
+++ Sem-1/C_Language/PRIME.C
@@ -3,14 +3,14 @@
 
 int isPrime(int);
 
-int main(){
-	int i,n,c=0;
+int main() {
+	int i, n, c = 0;
 	clrscr();
 
 	printf("\n\tEnter number: ");
-	scanf("%d",&n);
-	for(i=2;i<=n;i++){
-		if(isPrime(i)){
+	scanf("%d", &n);
+	for (i = 2;i <= n;i++) {
+		if (isPrime(i)) {
 			printf("%d, ", i);
 			c++;
 		}
@@ -26,10 +26,13 @@ int main(){
 	return 0;
 }
 
-int isPrime(int n){
+int isPrime(int n) {
 	int i;
-	for(i=2;i<=n/2;i++){
-		if(n%i == 0)
+	if (n <= 1) {
+		return 0;
+	}
+	for (i = 2;i <= n / 2;i++) {
+		if (n % i == 0)
 			return 0;
 	}
 	return 1;
