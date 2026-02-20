7ce2888d445d6b7d8600a267dcc6e4d5835efa1b
Chudasama Vishal <118047155+VishalChudasama08@users.noreply.github.com>
2026-02-21 00:14:12 +0530

    python unit-3 program 11-15 added

--- Sem-2/Python/Unit-3/UDF/11-12_prime.py
+++ Sem-2/Python/Unit-3/UDF/11-12_prime.py
@@ -1,6 +1,7 @@
 # Unit-3 PYTHON INDICATIVE LIST.docx
 
 # 11 Create a function to generate prime numbers. Ask total numbers form the user and pass in the function which will return a list of prime numbers.
+# 12 Eg. GeneratePrime(10) function will return 1st 10 prime numbers starting from 2 like  2,3,5,7,11,13,15,1719,23
 
 def isprime(n):
     ans = True
@@ -26,3 +27,4 @@ def generatePrime(n):
             
 print(generatePrime(int(input("How many prime number generate: "))))
 
+generatePrime(10);
---
+++ Sem-2/Python/Unit-3/UDF/13-16.py
@@ -0,0 +1,22 @@
+# Unit-3 PYTHON INDICATIVE LIST.docx
+
+# 13	Create a lambda function that will return maximum of two numbers
+
+max = lambda x,y: x if x>y else y
+print(max(10, 20))
+    
+# 14	Create a lambda function that will return maximum of three numbers
+
+max_3 = lambda x,y,z: x if x>y and x>z else(y if y>z else z)
+max_3(15, 20, 10)
+
+# 15	Write a lambda function that takes one number and if the number is even, returns that number multiplied by 5 else if the number is odd, returns that number multiplied by 10
+
+evenodd = lambda n=int(input("Enter number:")) : n*5 if n%2==0 else n*10;
+print(evenodd())
+
+"""
+16	Take a list of mixed elements and 
+    a.	Write a lambda function to separate integer elements as an output list. 
+    b.	Write another lambda function to separate string elements as an output list.
+"""
